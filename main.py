ca8e1547b709bf3714518e69ea3ded621a429932
jacquesnoguess <148258704+jacquesnoguess@users.noreply.github.com>
2024-09-26 21:32:51 +0000

    done

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 def binary_to_decimal(binary_str):
     value = 0
+    for i in range(7,-1,-1):
+        num = binary_str[7-i]
+        if num == "1":
+            value+=2**i
+    return value
+
+def decimal_to_binary(number):
+    binary = ""
+    exponent = 7
     for i in range(8):
-        num = binary_str[i]
-        if num == 1:
-            value+=
+        if number >= 2**exponent:
+            binary+='1'
+            number -= 2**exponent
+        else:
+            binary += '0'
+        exponent -= 1
+    return binary
+
