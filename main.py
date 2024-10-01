d931332a45ea304b50e553274b184789af026a14
jacquesnoguess <148258704+jacquesnoguess@users.noreply.github.com>
2024-10-01 20:21:04 +0000

    done

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 def binary_to_decimal(binary_str):
     value = 0
-    for i in range(7,-1,-1):
-        num = binary_str[7-i]
+    for i in range(8):
+        num = binary_str[7 - i]
         if num == "1":
             value+=2**i
     return value
-
+#binary_to_decimal("11111111")
 def decimal_to_binary(number):
     binary = ""
     exponent = 7
@@ -17,4 +17,5 @@ def decimal_to_binary(number):
             binary += '0'
         exponent -= 1
     return binary
+#decimal_to_binary(255)
 
